c8afdb3045aa1e465a496ed564062b90c1ca1c26
Utkarsh 2700 <utkarshsharma2718@gmail.com>
2024-07-10 15:18:21 +0530

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,21 @@
+// GLOBALS   - NO WINDOW !!!
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules (CommonJS)
+// module     - info about current module (file)
+// process    - info about env where the program is being executed
+
+// console.log(`path name =   ${__dirname}`);
+// console.log(`filename =   ${__filename}`);
+// console.log(`require=   ${require}`);
+// console.log(reqiure);
+// console.log(`module =   ${module}`);
+// console.log(module);
+// console.log(`process =   ${process}`);
+// console.log(process);
+console.log(__dirname);
+
+setInterval(() => {
+  console.log("hello world");
+}, 1000);
